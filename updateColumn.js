bd893c7123d3339dab2c3b2b50751f8b6ec0d44c
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-30 02:52:17 +0530

    Update updateColumn.js

--- updateColumn.js
+++ updateColumn.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.query("ALTER TABLE users MODIFY COLUMN age BIGINT;", (err, results) => {
+connection.query("ALTER TABLE projects ADD purchase_jobno VARCHAR(255) after jobno;", (err, results) => {
   if (err) throw err;
   console.log("Column updated!", results);
   connection.end();
