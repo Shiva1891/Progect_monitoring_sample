b897dcee9a896407611fc23e1106842fc803a588
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-29 08:05:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -20,5 +18,6 @@ const server = app.listen(PORT, () => {
 process.on("exit", () => {
   console.log("Process exiting");
 });
+
 
 
