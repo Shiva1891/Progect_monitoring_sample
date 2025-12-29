f8ecb61f5b32171be77e2edd2ebd7a93478c5500
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-30 04:03:23 +0530

    Delete updateColumn.js

--- updateColumn.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-
-connection.query("select * from projects", (err, results) => {
-  if (err) throw err;
-  console.log("Column updated!", results);
-  connection.end();
-});
