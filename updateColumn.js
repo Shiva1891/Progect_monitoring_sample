b37a0474c289e855115ac5acc33621991656855b
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-30 03:04:00 +0530

    Update updateColumn.js

--- updateColumn.js
+++ updateColumn.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.query("ALTER TABLE projects MODIFY COLUMN project_type JSON NULL;ALTER TABLE projects MODIFY COLUMN enquery_date JSON NULL;", (err, results) => {
+connection.query("ALTER TABLE projects MODIFY COLUMN quantity JSON NULL;ALTER TABLE projects MODIFY COLUMN expected_date JSON NULL;", (err, results) => {
   if (err) throw err;
   console.log("Column updated!", results);
   connection.end();
