694ff6d4cde8c88849e31b24dffb57bafa1e9f95
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-30 03:01:19 +0530

    Modify project_type and enquery_date columns to JSON

--- updateColumn.js
+++ updateColumn.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.query("ALTER TABLE projects ADD purchase_jobno VARCHAR(255) after jobno;", (err, results) => {
+connection.query("ALTER TABLE projects MODIFY COLUMN project_type JSON NULL;ALTER TABLE projects MODIFY COLUMN enquery_date JSON NULL;", (err, results) => {
   if (err) throw err;
   console.log("Column updated!", results);
   connection.end();
