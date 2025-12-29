cbca039431c79e1c9f67c879d73e2435a44e39f5
Shiva1891 <65371716+Shiva1891@users.noreply.github.com>
2025-12-29 07:28:40 +0530

    Format index.js for consistent styling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -10,6 +11,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
-
 
 
